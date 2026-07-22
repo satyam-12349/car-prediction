ad058a5d7bebd4ee6aee35244e62d9cf0eb8b2b9
satyam-12349 <satyam8840520725@gmail.com>
2026-07-22 11:11:48 +0530

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,15 +1,13 @@
-
 import pandas as pd 
 import numpy as np 
 import pickle as pk 
 import streamlit as st
 
-
-model = pk.load(open("model.pkl", "rb"))
+model = pk.load(open('model.pkl','rb'))
 
 st.header('Car Price Prediction ML Model')
 
-cars_data = pd.read_csv('Cardetails.csv')
+cars_data = pd.read_csv('cardetails.csv')
 
 def get_brand_name(car_name):
     car_name = car_name.split(' ')[0]
@@ -34,19 +32,18 @@ if st.button("Predict"):
     [[name,year,km_driven,fuel,seller_type,transmission,owner,mileage,engine,max_power,seats]],
     columns=['name','year','km_driven','fuel','seller_type','transmission','owner','mileage','engine','max_power','seats'])
     
-    input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
+    input_data_model['owner'] = input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
        'Fourth & Above Owner', 'Test Drive Car'],
-                           [1,2,3,4,5], inplace=True)
-    input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'],[1,2,3,4], inplace=True)
-    input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'],[1,2,3], inplace=True)
-    input_data_model['transmission'].replace(['Manual', 'Automatic'],[1,2], inplace=True)
-    input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
+                           [1,2,3,4,5])
+    input_data_model['fuel'] = input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'],[1,2,3,4])
+    input_data_model['seller_type'] = input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'],[1,2,3])
+    input_data_model['transmission'] = input_data_model['transmission'].replace(['Manual', 'Automatic'],[1,2])
+    input_data_model['name'] = input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
        'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
        'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
        'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
        'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
-                          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-                          ,inplace=True)
+                          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31])
 
     car_price = model.predict(input_data_model)
 
